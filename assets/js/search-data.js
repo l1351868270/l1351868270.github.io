8c57c9a55bd53e5a8c6012c3e2fcb7e90cc129f9
l1351868270 <32282854+l1351868270@users.noreply.github.com>
2026-02-28 08:31:17 +0000

    Deploying to gh-pages from @ l1351868270/l1351868270.github.io@fa3fa015777d452b263dbcfbfa04c12de5087b61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-first",
+        },{id: "post-first",
         
           title: "First",
         
